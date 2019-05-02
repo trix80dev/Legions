eecaa16c22b7be6cf45812cd8630826c133cd3c9
zzolr <49467061+zzolr@users.noreply.github.com>
2019-05-01 18:43:33 -0700

    Create Main.java
    Main file

---
+++ src/me/zZolt/Legions/Main.java
@@ -0,0 +1,63 @@
+package me.zZolt.Legions;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	@Override
+	public void onEnable()
+	{
+		this.getLogger().info("Legions has been enabled.!");
+		this.getServer().getPluginManager().registerEvents(new LegionsEntityDamageEventListener(this), this);
+		
+		
+		for (Player player: getServer().getOnlinePlayers()) {
+			
+		LegionsEntityDamageEventListener.playerEnergy.put(player.getDisplayName(), 0);
+		
+		}
+		
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
+		    public void run(){
+		    	
+		    	for (Player player: getServer().getOnlinePlayers()) {
+		    	
+		    	LegionsEntityDamageEventListener.sendActionBar(player);
+		    }
+		    	
+		    }
+		}, 0, 10);
+		
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+		this.getLogger().info("Legions has been disabled.");
+		
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		
+		Player player = event.getPlayer();
+		
+		if(LegionsEntityDamageEventListener.playerEnergy.containsKey(player.getDisplayName())){
+			
+			return;
+			
+		} else {
+		
+		LegionsEntityDamageEventListener.playerEnergy.put(player.getDisplayName(), 0);
+		
+		}
+	}
+		
+}
+	
